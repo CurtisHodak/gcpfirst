7ca673aea3bc5f7c82914fdde0e31b100961ebd0
Curtis Hodak <curtish@spacelift.com>
2025-08-08 15:00:27 -0400

    testing worker pools

--- main.tf
+++ main.tf
@@ -19,3 +19,24 @@
 # resource "random_id" "suffix" {
 #   byte_length = 3
 # }
+
+
+module "my_workerpool" {
+  source = "github.com/spacelift-io/terraform-google-spacelift-workerpool?ref=v1.2.0"
+
+  configuration = <<-EOT
+    export SPACELIFT_TOKEN="${var.worker_pool_config}"
+    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
+  EOT
+
+  image   = "projects/spacelift-workers/global/images/spacelift-worker-us-1634112379-tmoys2fp"
+  network = "default"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+  size    = 2
+  email   = "abc@xyz.iam.gserviceaccount.com"
+  
+  providers = {
+    google = google
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "worker_pool_config" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "worker_pool_private_key" {
+  type        = string
+  default     = ""
+  description = "description"
+}
